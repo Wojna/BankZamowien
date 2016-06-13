92c687354ac7b96f4f0b4b86224d9de102458af3
DamianW <damian.woinski@gmail.com>
2016-06-13 15:40:28 +0200

    Initial Commit

---
+++ BankZamowien/BankZamowien/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BankZamowien.MvcApplication" Language="C#" %>
---
+++ BankZamowien/BankZamowien/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BankZamowien.Startup))]
+namespace BankZamowien
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
